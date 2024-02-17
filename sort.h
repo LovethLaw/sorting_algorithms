9a2809f65b763eff0f9af89fcf9352c9ca900065
LovethLaw <talktoawi@gmail.com>
2024-02-17 04:09:42 -0800

    insertion

--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,5 +19,6 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 #endif
 
